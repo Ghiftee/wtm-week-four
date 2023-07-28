4f2de891b53b1a2aba428550dd073b62fcd850ad
Ghiftee <gigibetine@gmail.com>
2023-07-28 10:50:51 +0100

    Write scripts

---
+++ index.js
@@ -0,0 +1,30 @@
+let num1 = 6;
+let num2 = 2;
+
+// Multiplication
+function multiply(num1, num2){
+   return num1 * num2
+}
+
+console.log(multiply(num1, num2));
+
+//Addition
+function addition(num1, num2){
+    return num1 + num2
+}
+
+console.log(addition(num1, num2));
+
+// Division
+function divide(num1, num2){
+    return num1 / num2
+}
+
+console.log(divide(num1, num2));
+
+// Modulus
+function modulus(num1, num2){
+    return num1 % num2
+}
+
+console.log(modulus(num1, num2));
